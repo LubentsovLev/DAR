e993efdba7faa0e288ea82e37f59cfef00172b89
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2020-11-30 09:19:33 +0300

    mobile almost done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-$(function(){
+$(function () {});
 
+let burger = document.querySelector(".burger__burger");
+let nav = document.querySelector(".header__menu-container");
 
-
-  
+burger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  nav.classList.toggle("nav_active");
 });
