b3f9f4a6137b5d5286541f2148d318d41e68818d
lev_lubentsov <63476136+levheydude@users.noreply.github.com>
2020-12-01 17:37:52 +0300

    form and small things

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-$(function () {});
+$(function () {
+  $("form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+});
 
 let burger = document.querySelector(".burger__burger");
 let nav = document.querySelector(".header__menu-container");
